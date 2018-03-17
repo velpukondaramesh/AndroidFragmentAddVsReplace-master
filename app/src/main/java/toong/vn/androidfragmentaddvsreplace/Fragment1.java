243116592d34e2985b4ebb287abb26892ee49507
velpukondaramesh <velpukondaramesh@gmail.com>
2018-03-17 11:17:17 +0530

    sample project about fragment transaction using backstack

---
+++ app/src/main/java/toong/vn/androidfragmentaddvsreplace/Fragment1.java
@@ -0,0 +1,42 @@
+package toong.vn.androidfragmentaddvsreplace;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends BaseFragment {
+
+    public static Fragment1 newInstance() {
+        return new Fragment1();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView");
+        View rootView = inflater.inflate(R.layout.fragment_1, null, false);
+
+        rootView.findViewById(R.id.button_pop).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getFragmentManager().popBackStack();
+                //getFragmentManager().popBackStack(mFrag1Id, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            }
+        });
+
+        rootView.findViewById(R.id.button_go_to_fragment_2)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ((MainActivity)getActivity()).goToFragment(Fragment2.newInstance());
+                    }
+                });
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/toong/vn/androidfragmentaddvsreplace/Fragment3.java
@@ -0,0 +1,36 @@
+package toong.vn.androidfragmentaddvsreplace;
+
+import android.app.FragmentManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment3 extends BaseFragment {
+
+    public static Fragment3 newInstance() {
+        return new Fragment3();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView");
+        View rootView = inflater.inflate(R.layout.fragment_3, null, false);
+
+        rootView.findViewById(R.id.button_pop_without_animation)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        getFragmentManager().popBackStack();
+                        // fragment transaction by using tag
+                        //getFragmentManager().popBackStack("Fragment1", FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                    }
+                });
+
+        return rootView;
+    }
+}
